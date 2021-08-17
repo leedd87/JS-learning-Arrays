bfeb842f4d367feff959dac6afeed5297436370e
Lee David <leedav87@gmail.com>
2021-08-17 14:17:52 -0300

    Repaso de Class/Objeto

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-let numero = 10;
+class Prueba {
+    constructor(nombre, apellido, edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.familia = familia;
+    }
+    visualizar() {
+        console.log(this);
+    }
+}
 
-console.log(numero);
+this.direccion = prompt('pasame tu direccion');
+this.familia = prompt('cuantas personas son en tu familia');
+
+let david = new Prueba('david', 'lee', 34)
+
+console.log(david)
