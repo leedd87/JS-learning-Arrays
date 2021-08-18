957139110aff259ff79680e0475e522f70c9240c
Lee David <leedav87@gmail.com>
2021-08-18 13:37:24 -0300

    probando arrays

--- js/script.js
+++ js/script.js
@@ -19,16 +19,65 @@
 // console.log(david)
 
 
-const listaNombre = [];
+// const listaNombre = [];
 
-let cantidad = 5;
+// let cantidad = 5;
 
-do {
-    let entrada = prompt('pasame nombre');
-    listaNombre.push(entrada.toUpperCase());
-    console.log(listaNombre);
-} while (listaNombre.length == cantidad)
+// do {
+//     let entrada = prompt('pasame nombre');
+//     listaNombre.push(entrada.toUpperCase());
+//     console.log(listaNombre);
+// } while (listaNombre.length != cantidad)
 
-const nuevaListaNombre = listaNombre.concat(['ana', 'grace']);
+// const nuevaListaNombre = listaNombre.concat(['ana', 'grace']);
 
-console.log(nuevaListaNombre);
+// console.log(nuevaListaNombre);
+
+//Incorporando Arrays
+
+class Hotel {
+    constructor(hotel, habitacion, servicio) {
+        this.hotel = hotel;
+        this.habitacion = habitacion;
+        this.servicio = servicio;
+    }
+    visualizar() {
+        console.log(`Seleccionaste las siguientes opciones: ${hotel}, ${habitacion} y ${servicio}`);
+    }
+}
+
+let preguntarHotel = prompt('Elegi un hotel: Punta Cana, Dominica, Grand Bavaro');
+
+if (preguntarHotel == 'Punta Cana' || 'punta cana') {
+    console.log('seleccionaste punta cana');
+} else {
+    console.log('no entendi');
+}
+
+let preguntarHabitacion = prompt('Elegi tipo de habitacion: Suite, Doble, Triple');
+let preguntarServicio = prompt('Elegi tipo de servicio: All Inclusive, Media Pension, Solo Desayuno');
+
+let seleccionUsuario1 = new Hotel(preguntarHotel);
+
+console.log(seleccionUsuario1);
+
+
+let preguntarHotel2 = prompt('Elegi un hotel: Punta Cana, Dominica, Grand Bavaro');
+let preguntarHabitacion2 = prompt('Elegi tipo de habitacion: Suite, Doble, Triple');
+let preguntarServicio2 = prompt('Elegi tipo de servicio: All Inclusive, Media Pension, Solo Desayuno');
+
+let seleccionUsuario2 = new Hotel(preguntarHotel2, preguntarHabitacion2, preguntarServicio2);
+
+console.log(seleccionUsuario2);
+
+let preguntarHotel3 = prompt('Elegi un hotel: Punta Cana, Dominica, Grand Bavaro');
+let preguntarHabitacion3 = prompt('Elegi tipo de habitacion: Suite, Doble, Triple');
+let preguntarServicio3 = prompt('Elegi tipo de servicio: All Inclusive, Media Pension, Solo Desayuno');
+
+let seleccionUsuario3 = new Hotel(preguntarHotel3, preguntarHabitacion3, preguntarServicio3);
+
+console.log(seleccionUsuario3);
+
+const totalSeleccionUsuario = [seleccionUsuario1, seleccionUsuario2, seleccionUsuario3];
+
+console.log(totalSeleccionUsuario);
