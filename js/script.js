e0a5787d5bb9ca80dc3b563c11b08b0cc89234a5
Lee David <leedav87@gmail.com>
2021-08-18 07:18:40 -0300

    probando class y metodos de class

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-class Prueba {
-    constructor(nombre, apellido, edad, direccion, familia) {
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.direccion = direccion;
-        this.familia = familia;
-    }
-    visualizar() {
-        console.log(this);
-    }
-}
+// class Prueba {
+//     constructor(nombre, apellido, edad, direccion, familia) {
+//         this.nombre = nombre;
+//         this.apellido = apellido;
+//         this.edad = edad;
+//         this.direccion = direccion;
+//         this.familia = familia;
+//     }
+//     visualizar() {
+//         console.log(this);
+//     }
+// }
 
-let pideDireccion = prompt('pasame tu direccion');
-let pideFamilia = prompt('cuantas personas son en tu familia');
+// let pideDireccion = prompt('pasame tu direccion');
+// let pideFamilia = prompt('cuantas personas son en tu familia');
 
-let david = new Prueba('david', 'lee', 34, pideDireccion, pideFamilia)
+// let david = new Prueba('david', 'lee', 34, pideDireccion, pideFamilia)
 
-console.log(david)
+// console.log(david)
+
+
+const listaNombre = [];
+
+let cantidad = 5;
+
+do {
+    let entrada = prompt('pasame nombre');
+    listaNombre.push(entrada.toUpperCase());
+    console.log(listaNombre);
+} while (listaNombre.length == cantidad)
+
+const nuevaListaNombre = listaNombre.concat(['ana', 'grace']);
+
+console.log(nuevaListaNombre);
